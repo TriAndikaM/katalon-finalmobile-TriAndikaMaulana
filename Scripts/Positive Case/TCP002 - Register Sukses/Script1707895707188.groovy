f631b8de4bf8ade87b72cd3d6b1ba5a4bc109857
TriAndikaM <triandika3@gmail.com>
2024-02-14 16:42:22 +0700

    Create login and register test case

---
+++ Scripts/Positive Case/TCP002 - Register Sukses/Script1707895707188.groovy
@@ -0,0 +1,57 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\asus\\Downloads\\Documents\\Compressed\\Advantage+demo+3.2.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/registerpage/swipebar'), 10)
+
+Mobile.tap(findTestObject('Object Repository/registerpage/loginwidget'), 10)
+
+Mobile.tap(findTestObject('Object Repository/registerpage/signup today button'), 10)
+
+Mobile.setText(findTestObject('Object Repository/registerpage/usernameregister.EditText'), 'tikus', 10)
+
+Mobile.setText(findTestObject('Object Repository/registerpage/emailregister.EditText'), 'tikus@gmail.com', 10)
+
+Mobile.setText(findTestObject('Object Repository/registerpage/passowrdregister.EditText'), 'Tikus1', 10)
+
+Mobile.setText(findTestObject('Object Repository/registerpage/confirmpassregister.EditText'), 'Tikus1', 10)
+
+Mobile.hideKeyboard()
+
+Mobile.setText(findTestObject('Object Repository/registerpage/firstname'), 'Tikus', 10)
+
+Mobile.hideKeyboard()
+
+Mobile.scrollToText('City')
+
+Mobile.setText(findTestObject('Object Repository/registerpage/address'), 'Gorong gorong', 10)
+
+Mobile.hideKeyboard()
+
+Mobile.delay(1)
+
+Mobile.scrollToText('REGISTER')
+
+Mobile.tap(findTestObject('Object Repository/registerpage/Button - REGISTER'), 10)
+
+Mobile.delay(2)
+ 
+Mobile.closeApplication()
+
